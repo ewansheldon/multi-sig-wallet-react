5045843e5862fe5850ddd06e46830a3f4fc4f75b
ewansheldon <ewansheldon@gmail.com>
2022-02-21 21:41:59 +0000

    connect to metamask and update address

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,45 @@
 import React from 'react';
 import './App.css';
 import {Button, Message} from "semantic-ui-react";
+import {useWeb3Context} from "./contexts/Web3";
+import {unlockAccount} from "./api/web3";
+import useAsync from "./components/useAsync";
 
 function App() {
-  const account = "0x324235";
-  return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Multi Sig Wallet</h1>
-        <div>Account: {account}</div>
+  const {
+    state: {account},
+    updateAccount
+  } = useWeb3Context();
 
-        <Message warning>Metamask is not connected</Message>
-        <Button color="green">Connect to metamask</Button>
+  const {pending, error, call} = useAsync(unlockAccount);
+
+  async function onClickConnect() {
+    const {error, data} = await call(null);
+    if (error) {
+      console.error(error);
+    }
+    if (data) {
+      updateAccount(data);
+    }
+  }
+
+  return (
+      <div className="App">
+        <div className="App-header">
+          <h1>Multi Sig Wallet</h1>
+          <div>Account: {account}</div>
+
+          <Message warning>Metamask is not connected</Message>
+          <Button
+              color="green"
+              onClick={() => onClickConnect()}
+              disabled={pending}
+              loading={pending}
+          >
+            Connect to metamask
+          </Button>
+        </div>
       </div>
-    </div>
   );
 }
 
